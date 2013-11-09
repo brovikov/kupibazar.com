14f9cfbc176abafc2584a6f9501de9c74dbc71e8
Alexey Brovikov <alexey.brovikov@gmail.com>
2013-11-09 20:03:55 +0000

    Kupibazar v1.0.0 without admin restr

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,18 +1,3 @@
 module OrdersHelper
 
-  def order_stat (status)
-    case
-      when status == 0 
-        '<span class="label label-warning"> На согласовании </span></br>'.html_safe
-      when status == 1 
-        '<span class="label label-success"> Подтвержден </span></br>'.html_safe
-      when status == 2 
-        '<span class="label label-success"> Оплачен </span></br>'.html_safe
-      when status == 9 
-        '<span class="label label-important"> Отклонен </span></br>'.html_safe
-      when status == 7 
-        '<span class="label label-success"> Выкуплен </span></br>'.html_safe
-      end     
-  end
-  
 end
